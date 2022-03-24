b3e6416910343c7ef540e10a1f46cab1947d8ee9
yc11241124 <82065959+yc11241124@users.noreply.github.com>
2022-03-23 21:11:36 -0400

    Add files via upload

---
+++ 3.23BigQueryUsageDashboard/TableSchema.sql
@@ -0,0 +1,8 @@
+SELECT table_schema
+FROM
+region-us.INFORMATION_SCHEMA.TABLES AS tables
+LEFT JOIN
+bq_audits.bq_query_audit_tables AS audit_tables
+	ON tables.table_catalog= audit_tables.projectId AND tables.table_schema= audit_tables.datasetId AND tables.table_name= audit_tables.tableId
+	AND CASE tables.table_type WHEN 'BASE TABLE' THEN audit_tables.table_type ='table' WHEN 'VIEW' THEN  audit_tables.table_type ='view' END
+WHERE DATE(createTime) >= CURRENT_DATE() - 90
