ee174f1ec0bbcc543dcdc28a896bd1ce56a7c2fd
yc11241124 <82065959+yc11241124@users.noreply.github.com>
2022-03-28 23:18:03 -0400

    Update Table_not_used.sql

--- 3.23BigQueryUsageDashboard/Table_not_used.sql
+++ 3.23BigQueryUsageDashboard/Table_not_used.sql
@@ -1,8 +1,14 @@
-SELECT max(createTime) as create_time, table_name
-FROM bq_audits.bq_query_audit_tables AS audit_tables
+SELECT
+    max(createTime) as LastQuerydate, table_name, count(*) as cnt
+FROM
+    bq_audits.bq_query_audit_tables AS audit_tables
 LEFT JOIN
-region-us.INFORMATION_SCHEMA.TABLES AS tables
+    region-us.INFORMATION_SCHEMA.TABLES AS tables
 ON tables.table_catalog= audit_tables.projectId AND tables.table_schema= audit_tables.datasetId AND tables.table_name= audit_tables.tableId
-AND CASE tables.table_type WHEN 'BASE TABLE' THEN audit_tables.table_type ='table' WHEN 'VIEW' THEN  audit_tables.table_type ='view' END
-WHERE NOTDATE(createTime) >= CURRENT_DATE() - 90
-GROUP BY table_name
+    AND CASE tables.table_type WHEN 'BASE TABLE' THEN audit_tables.table_type ='table' WHEN 'VIEW' THEN  audit_tables.table_type ='view' END
+GROUP BY
+    table_name
+HAVING
+    MAX(DATE(createTime)) <= CURRENT_DATE() - 90
+ORDER BY 
+    count(*) DESC
